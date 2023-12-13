7f15828f1f2aff51b9d403528d69d3978fa74f59
audrey-bunn <124309333+audrey-bunn@users.noreply.github.com>
2023-12-13 21:32:29 +0000

    Added match checking and messaging

--- src/script.js
+++ src/script.js
@@ -1,4 +1,27 @@
 function checkMatch(event) {
   event.currentTarget.classList.add('selected');
+  var selectedItems = document.getElementsByClassName('selected');
   
+  if(selectedItems.length > 1) {
+    var isMatch = selectedItems[0].name === selectedItems[1].name
+    console.log(isMatch);
+    if(isMatch) {
+      document.getElementById('success').style.display = 'block';
+      document.getElementById('success').innerHTML = document.getElementById('success').innerHTML + selectedItems[0].name;
+      setTimeout(() => {      document.getElementById('success').style.display = 'none';
+;},'2000');
+      
+      selectedItems[0].classList.add('matched');
+      selectedItems[1].classList.add('matched');
+      selectedItems[0].classList.remove('selected');
+      selectedItems[0].classList.remove('selected');
+    }
+    else {
+      document.getElementById('failure').style.display = 'block';
+      setTimeout(() => {      document.getElementById('failure').style.display = 'none';
+                       selectedItems[0].classList.remove('selected');
+                       selectedItems[0].classList.remove('selected');},'2000');
+
+    }
+  }
 }
