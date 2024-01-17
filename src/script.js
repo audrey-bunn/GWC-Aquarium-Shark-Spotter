4f6066623fa94acf379fd3699c2593fc78caa69a
audrey-bunn <124309333+audrey-bunn@users.noreply.github.com>
2024-01-17 21:06:49 +0000

    fixed bugs - extra names on success message and not waiting for message to go away before checking new matches

--- src/script.js
+++ src/script.js
@@ -1,4 +1,10 @@
 function checkMatch(event) {
+  isMessageDisplayed = document.getElementById('success').style.display === 'block' || document.getElementById('failure').style.display === 'block';
+
+  if(isMessageDisplayed) {
+    return;
+  }
+  
   event.currentTarget.classList.add('selected');
   var selectedItems = document.getElementsByClassName('selected');
   
@@ -7,8 +13,12 @@ function checkMatch(event) {
     console.log(isMatch);
     if(isMatch) {
       document.getElementById('success').style.display = 'block';
+      
+      successMessage = document.getElementById('success').innerHTML;
+      
       document.getElementById('success').innerHTML = document.getElementById('success').innerHTML + selectedItems[0].name;
-      setTimeout(() => {      document.getElementById('success').style.display = 'none';
+      
+      setTimeout(() => {      document.getElementById('success').style.display = 'none';                document.getElementById('success').innerHTML = successMessage;
 ;},'2000');
       
       selectedItems[0].classList.add('matched');
