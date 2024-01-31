004e7ff455a6ea86a0176513b253f032f6dc1c42
audrey-bunn <124309333+audrey-bunn@users.noreply.github.com>
2024-01-31 20:32:30 +0000

    matching logic and ui

--- src/script.js
+++ src/script.js
@@ -1,36 +1,47 @@
 function checkMatch(event) {
   isMessageDisplayed = document.getElementById('success').style.display === 'block' || document.getElementById('failure').style.display === 'block';
 
-  if(isMessageDisplayed) {
+  if (isMessageDisplayed) {
     return;
   }
-  
+
   event.currentTarget.classList.add('selected');
   var selectedItems = document.getElementsByClassName('selected');
-  
-  if(selectedItems.length > 1) {
+
+  if (selectedItems.length > 1) {
     var isMatch = selectedItems[0].name === selectedItems[1].name
     console.log(isMatch);
-    if(isMatch) {
+    if (isMatch) {
       document.getElementById('success').style.display = 'block';
-      
+
       successMessage = document.getElementById('success').innerHTML;
-      
+
       document.getElementById('success').innerHTML = document.getElementById('success').innerHTML + selectedItems[0].name;
-      
-      setTimeout(() => {      document.getElementById('success').style.display = 'none';                document.getElementById('success').innerHTML = successMessage;
-;},'2000');
-      
-      selectedItems[0].classList.add('matched');
-      selectedItems[1].classList.add('matched');
-      selectedItems[0].classList.remove('selected');
-      selectedItems[0].classList.remove('selected');
+
+      setTimeout(() => {
+        document.getElementById('success').style.display = 'none'; document.getElementById('success').innerHTML = successMessage;
+
+        selectedItems[0].classList.add('matched');
+        selectedItems[1].classList.add('matched');
+        selectedItems[0].classList.remove('selected');
+        selectedItems[0].classList.remove('selected');
+        ;
+      }, '2500');
+
     }
     else {
       document.getElementById('failure').style.display = 'block';
-      setTimeout(() => {      document.getElementById('failure').style.display = 'none';
-                       selectedItems[0].classList.remove('selected');
-                       selectedItems[0].classList.remove('selected');},'2000');
+      selectedItems[0].classList.add('incorrect');
+      selectedItems[1].classList.add('incorrect');
+      setTimeout(() => {
+        document.getElementById('failure').style.display = 'none';
+
+        selectedItems[0].classList.remove('incorrect');
+        selectedItems[1].classList.remove('incorrect');
+        selectedItems[0].classList.remove('selected');
+        selectedItems[0].classList.remove('selected');
+
+      }, '2500');
 
     }
   }
